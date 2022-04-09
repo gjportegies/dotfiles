13b2b5c216feb050684d2e0cfe7dbd99e666b8cd
Guus Portegies <guus@ceesenco.com>
2022-04-09 13:48:07 +0200

    Added aliases.zsh file

---
+++ aliases.zsh
@@ -0,0 +1,5 @@
+# Shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
+alias reloadshell="source $HOME/.zshrc"
+alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
+alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
