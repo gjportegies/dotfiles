16d595ef7d7da0f6d073daf62554a743aceb1fb3
Guus Portegies <guus@ceesenco.com>
2022-04-09 13:52:45 +0200

    Added extra aliases

--- aliases.zsh
+++ aliases.zsh
@@ -3,3 +3,10 @@ alias copyssh="pbcopy < $HOME/.ssh/id_ed25519.pub"
 alias reloadshell="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
+
+# Directories
+alias dotfiles="cd $DOTFILES"
+alias sites="cd $HOME/Sites"
+
+# Laravel
+alias a="php artisan"
